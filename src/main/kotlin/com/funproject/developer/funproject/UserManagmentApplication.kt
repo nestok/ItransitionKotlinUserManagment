da1904bb6f39607cdd968fdc94355d0a91e108ac
nestok <sych_y@mail.ru>
2018-08-27 17:58:42 +0300

    Added Eureka client dependencies

--- src/main/kotlin/com/funproject/developer/funproject/UserManagmentApplication.kt
+++ src/main/kotlin/com/funproject/developer/funproject/UserManagmentApplication.kt
@@ -2,8 +2,10 @@ package com.funproject.developer.funproject
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class UserManagmentApplication
 
 fun main(args: Array<String>) {
